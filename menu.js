b833f7a184f9bb4aad11b4fd227e02923121bb86
Sammy-E <82353135+Sammy-E@users.noreply.github.com>
2023-11-29 08:13:39 +0100

    Update menu.js

--- menu.js
+++ menu.js
@@ -13,7 +13,7 @@ const menu = [
     category: "lunch",
     price: 13.99,
     img: "./images/item-2.jpeg",
-    desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+    desc: `15 okra, 2 large peppers, 1 onion, small potash, 2 small bitterleaf tips, salt, and meat and water`,
   },
   {
     id: 3,
@@ -29,7 +29,7 @@ const menu = [
     category: "shakes",
     price: 6.99,
     img: "./images/item-3.jpeg",
-    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+    desc: `Turkey neck bones, water, onion, garlic, salt, black eyed peas, tomato paste, and oil.`,
   },
   {
     id: 4,
@@ -37,7 +37,7 @@ const menu = [
     category: "breakfast",
     price: 20.99,
     img: "./images/item-4.jpeg",
-    desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+    desc: `Small tin of tomato paste, onion powder, garlic powder, pepper, basil, mushrooms, diced bell tomatoes, and oil `,
   },
   {
     id: 5,
@@ -45,7 +45,7 @@ const menu = [
     category: "lunch",
     price: 22.99,
     img: "./images/item-5.jpeg",
-    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+    desc: `2 cups graines de tournesol, 1 onion, 4 tomatoes or 1 can of tomato purée, 2 hot peppers, 160 ml of oil, salt, and water `,
   },
   {
     id: 6,
@@ -53,7 +53,7 @@ const menu = [
     category: "shakes",
     price: 18.99,
     img: "./images/item-6.jpeg",
-    desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+    desc: `Salt, allspice, cloves, curry, onion purée, garlic purée, ginger, mango, and hot african peppers. Lime, rum, and basil.`,
   },
   {
     id: 7,
@@ -61,7 +61,7 @@ const menu = [
     category: "breakfast",
     price: 8.99,
     img: "./images/item-7.jpeg",
-    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+    desc: `curry, salt, pepper, garlic purée, onion purée, and mpu fish spice. Cover with basil, parsley, and bay leaf sauce.`,
   },
   {
     id: 8,
@@ -69,7 +69,7 @@ const menu = [
     category: "lunch",
     price: 12.99,
     img: "./images/item-8.jpeg",
-    desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+    desc: `lemon, salt, pepper, mpu fish spice, basil, and white wine.`,
   },
   {
     id: 9,
@@ -77,7 +77,7 @@ const menu = [
     category: "shakes",
     price: 16.99,
     img: "./images/item-9.jpeg",
-    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+    desc: `njangsa, bush pepper, basil, salt, pepper, freshly crushed garlic.`,
   },
 ];
 
